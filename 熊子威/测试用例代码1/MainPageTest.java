bb1d33b69ab59ec6e85addacbf5e6380b808964a
熊子威 <44659974+2018011873@users.noreply.github.com>
2021-05-26 11:03:39 +0800

    任务

--- 熊子威/测试用例代码1/MainPageTest.java
+++ 熊子威/测试用例代码1/MainPageTest.java
@@ -20,122 +20,122 @@ public class MainPageTest extends BaseDriver {
 	@Test(priority = 1)
 	public void test_skip_fail1() {
 		String message = mainPage.skip_fail1();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 
 	}
 
 	@Test(priority = 2)
 	public void test_skip_fail2() {
 		String message = mainPage.skip_fail2();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 3)
 	public void test_skip_fail3() {
 		String message = mainPage.skip_fail3();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 4)
 	public void test_skip_fail4() {
 		String message = mainPage.skip_fail4();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 5)
 	public void test_skip_fail5() {
 		String message = mainPage.skip_fail5();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 6)
 	public void test_skip_fail6() {
 		String message = mainPage.skip_fail6();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 7)
 	public void test_skip_fail7() {
 		String message = mainPage.skip_fail7();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 8)
 	public void test_skip_fail8() {
 		String message = mainPage.skip_fail8();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 9)
 	public void test_skip_fail9() {
 		String message = mainPage.skip_fail9();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 10)
 	public void test_skip_fail10() {
 		String message = mainPage.skip_fail10();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 11)
 	public void test_skip_fail11() {
 		String message = mainPage.skip_fail10();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 12)
 	public void test_skip_fail12() {
 		String message = mainPage.skip_fail10();
-		assertEquals(message, "ÕıÔÚ¿ª·¢ÖĞ");
+		assertEquals(message, "æ­£åœ¨å¼€å‘ä¸­");
 	}
 
 	@Test(priority = 13)
 	public void test_collect_success1() {
 		String message = mainPage.collect_success1();
-		assertEquals(message, "ÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "æ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 14)
 	public void test_collect_success2() {
 		String message = mainPage.collect_success2();
-		assertEquals(message, "ÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "æ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 15)
 	public void test_collect_success3() {
 		String message = mainPage.collect_success3();
-		assertEquals(message, "ÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "æ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 16)
 	public void test_collect_success4() {
 		String message = mainPage.collect_success4();
-		assertEquals(message, "ÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "æ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 17)
 	public void test_recollect_success1() {
 		String message = mainPage.recollect_success1();
-		assertEquals(message, "È¡ÏûÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "å–æ¶ˆæ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 18)
 	public void test_recollect_success2() {
 		String message = mainPage.recollect_success2();
-		assertEquals(message, "È¡ÏûÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "å–æ¶ˆæ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 19)
 	public void test_recollect_success3() {
 		String message = mainPage.recollect_success3();
-		assertEquals(message, "È¡ÏûÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "å–æ¶ˆæ”¶è—æˆåŠŸå•¦");
 	}
 
 	@Test(priority = 20)
 	public void test_recollect_success4() {
 		String message = mainPage.recollect_success4();
-		assertEquals(message, "È¡ÏûÊÕ²Ø³É¹¦À²");
+		assertEquals(message, "å–æ¶ˆæ”¶è—æˆåŠŸå•¦");
 	}
 
 }
